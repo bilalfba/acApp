0c011baec5c677e15e6f52fc3cc55390d9812192
Bilal Shabandri <bilalshabandri@Bilals-MacBook-Air.local>
2018-02-15 23:28:51 +0400

    Initial commit

---
+++ app/src/main/java/com/example/bilalshabandri/arcabapp/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.bilalshabandri.arcabapp;
+
+/**
+ * Created by bilalshabandri on 2/15/18.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lasemcode on 13/01/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
